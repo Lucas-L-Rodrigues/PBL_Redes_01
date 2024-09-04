2520a7e3fd7f53fe945360842202e99df90ec007
Lucas Lima <lucaslima.48a@gmai.com>
2024-09-04 11:21:28 -0300

    separando a classe usuario da classe passagem

--- classes.py
+++ classes.py
@@ -23,18 +23,3 @@ class Passagem:
     def realizarCancelamento(self):
         
         return
-
-class Usuario:
-    def __init__(self, login, senha):
-        self.login = login
-        self.senha = senha
-        self.passagens = []
-
-    def entrar(self, login, senha):
-        if (self.login==login and self.senha == senha):
-            print(f'logado com sucesso {login}')
-        else:
-            print(f'Não foi possivel logar, tente novamente')
-
-    def adicionarPassagem(self, passagem):
-        self.passagens.append(passagem)
