3ab81a5fb1c07acc514308ba45532ab71dbbaf95
Lucas Lima <lucaslima.48a@gmai.com>
2024-09-04 12:43:12 -0300

    Modificando o TCP_Servidor para receber credenciais de login e comparar com o banco de dados em JSON

--- TCP_servidor.py
+++ TCP_servidor.py
@@ -1,5 +1,46 @@
 import socket
 import threading
+import json
+
+def verificarLogin(login, senha, usuarios):
+    for usuario in usuarios:
+        if (usuario['login']==login and usuario['senha']==senha):
+            return True
+    return False
+
+def novoCliente(conn, addr):
+    print(f'Nova conexão com {addr}')
+    
+    while True:
+        try:
+            credenciais = conn.recv(1024)  # Recebe dados
+            if not credenciais:
+                break               # Sai do loop se o cliente não enviar nada, no caso digitando SAIR no TCP_Cliente
+
+            print(f'{addr} mandou: {credenciais.decode()}\n')   # Mensagem de confirmação
+            conn.sendall(b'O servidor recebeu as informacoes.')  # Envia resposta ao cliente
+
+        except ConnectionResetError:
+            break                       # Sai do loop se a conexão for resetada
+
+    conn.close()                        # Fecha a conexão ao sair do loop
+    print(f'Conexão encerrada: {addr}')
+
+# NÃO USADA
+def conectar_com_cliente(con, cliente):                     # Função para se comunicar com o cliente para troca de mensagens
+    print("Conectado com ", cliente)                        # Ta aqui so pra dar o feedback que a conexao aconteceu
+    while True:
+        try:
+            mensagem = con.recv(1024).decode()              # Recebe e decodifica a mensagem do cliente
+            if not mensagem:                                # Se a mensagem for uma string vazia
+                break                                       # Sai do loop, sem mensagem sem conexao
+            print(f"Cliente {cliente} enviou: \n{mensagem}") # printa a mensagem que recebeu do cliente
+        except ConnectionResetError:                        # ta aqui so por desencargo de consciencia mesmo caso alguma coisa aconteca
+            print(f"Cliente {cliente} desconectou inesperadamente.")
+            break
+
+    print(f"Conexão encerrada com {cliente}.")  # depois de receber a mensagem encerramos a conexao para evitar sobrecarregar o limite do tcp.listen()
+    con.close()                                 # Fecha a conexão com o cliente
 
 HOST = ''        # só declarando o host que vai ser usado na tupla "origem" 
 PORT = 8000      # Porta que vai ser usada na conexao
@@ -10,29 +51,17 @@ origem = (HOST, PORT)  # essa tupla "guarda" a informacao do servidor em que se 
 tcp.bind(origem)       # Ligando o socket a origem
 tcp.listen(10)         # Escutando até 10 conexões em fila
 
-# Funcao para se comunicar com um cliente
-def conectar_com_cliente(con, cliente):
-    print("Conectado com ", cliente)  # Ta aqui so pra dar o feedback que a conexao aconteceu
-    while True:
-        try:
-            mensagem = con.recv(1024).decode()  # Recebe e decodifica a mensagem do cliente
-            if not mensagem:                    # Se a mensagem for uma string vazia
-                break                           # Sai do loop, sem mensagem sem conexao
-            print(f"Cliente {cliente} enviou: \n{mensagem}") # printa a mensagem que recebeu do cliente
-        except ConnectionResetError:            # ta aqui so por desencargo de consciencia mesmo caso alguma coisa aconteca
-            print(f"Cliente {cliente} desconectou inesperadamente.")
-            break
-
-    print(f"Conexão encerrada com {cliente}.")  # depois de receber a mensagem encerramos a conexao para evitar sobrecarregar o limite do tcp.listen()
-    con.close()                                 # Fecha a conexão com o cliente
+tcp.listen(10)         # Escutando até 10 conexões em fila
 
 # Aqui eh a execucao do programa propriamente dita
 while True:
     con, cliente = tcp.accept()                     # Aceita uma nova conexão com "cliente"
     print(f"Nova conexão aceita de {cliente}")      # Mensagem para feedback
-    
-    client_thread = threading.Thread(target=conectar_com_cliente, args=(con, cliente)) # Criando uma nova thread para a conexao com "cliente"
-    client_thread.start()                                                              # Iniciando a thread
+
+    thread = threading.Thread(target=novoCliente, args=(con, cliente))          # Criando uma nova thread para a conexao com "cliente"
+    thread.start()                                                              # Iniciando a thread
+
+    print(f'Número de conexões ativas: {threading.active_count() - 1}')         # Mostra o número de threads ativas
 
 print("FIM")  # Esse print nao acontece na pratica, pois nao tem um break no while, mas caso no futuro apliquemos algo depois do fim da execucao.
 
